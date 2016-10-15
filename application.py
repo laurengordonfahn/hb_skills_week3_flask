ca737eecf57840027d0792e6329526ac5610a9b2
vagrant <vagrant@vagrant.vm>
2016-10-15 22:49:14 +0000

    Created All Files for HW. Working on getting method POST to run on applicatoin-forms page and subl command to work so I can add env/ to .gitignore

---
+++ application.py
@@ -0,0 +1,49 @@
+from flask import Flask, render_template, request, flash
+from flask_debugtoolbar import DebugToolbarExtension
+
+
+app = Flask(__name__)
+
+
+# Required to use Flask sessions and the debug toolbar
+app.secret_key = "ABC"
+
+@app.route("/")
+def index_page():
+    """Show an index page."""
+
+    # return "<html><body>This is the homepage.</body></html>"
+    # Alternately, we could make this a Jinja template in `templates/`
+    # and return that result of rendering this, like:
+    #
+    return render_template("index.html")
+
+@app.route("/application_form", methods=["POST"])
+def show_application_form():
+        flash("Thank you. Your application has been submitted.")
+        return render_template("application-form.html")
+
+
+@app.route("/application")
+def show_submitted_application():
+    first_name = request.form.get("firstname")
+    last_name = request.form.get("lastname")
+    salary = request.form.get("quantity")
+    job = request.form.get("job_title")
+    return render_template("application-response.html", 
+                            quantity= salary, 
+                            firstname = first_name, 
+                            lastname = last_name,
+                            job_title = job)
+
+
+if __name__ == "__main__":
+    # We have to set debug=True here, since it has to be True at the
+    # point that we invoke the DebugToolbarExtension
+    app.debug = True
+
+    # Use the DebugToolbar
+    DebugToolbarExtension(app)
+
+    app.run(host="0.0.0.0")
+
